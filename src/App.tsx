bd827a643a45c76a835c2839931fec8af7afab9d
Lukas <lukas@daangn.com>
2022-06-25 17:32:30 +0900

    add stackflow

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Stack } from './Stack/stackflow';
 
 function App() {
   return (
-    <Stack />
+    <div>
+      <Stack />
+    </div>
   );
 }
 
---
+++ src/Detail/DetailPage.tsx
@@ -0,0 +1,11 @@
+import {AppScreen} from "@stackflow/basic-ui";
+
+const DetailPage = () => {
+  return (
+    <AppScreen theme="cupertino" appBar={{ title: "상세 페이지" }}>
+      상세 페이지
+    </AppScreen>
+  )
+}
+
+export default DetailPage
