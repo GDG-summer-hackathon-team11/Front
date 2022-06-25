8bdacaf7059bb717f164a89fd09d58a0379c14e1
kmin-jeong <desk535353@gmail.com>
2022-06-25 22:55:01 +0900

    add:

--- src/Main/MainPage.tsx
+++ src/Main/MainPage.tsx
@@ -2,7 +2,7 @@ import { AppScreen } from "@stackflow/basic-ui";
 import {useFlow} from "../Stack/stackflow";
 import DynamicMap from "../Map/DynamicMap";
 import MapMarkerController from "../Map/MapMarkerController";
-
+import StaticMap from "../Map/staticMap"
 const MainPage = () => {
   const { push } = useFlow();
 
@@ -14,9 +14,9 @@ const MainPage = () => {
 
   return (
     <AppScreen theme="cupertino" appBar={{ title: "메인 페이지" }}>
-      <DynamicMap>
+      <StaticMap>
         <MapMarkerController />
-      </DynamicMap>
+      </StaticMap>
       메인 페이지
       <button onClick={onClick}>상세 페이지 이동</button>
     </AppScreen>
