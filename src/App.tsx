418500671c8aa638ccc952d6ae12263091d04ba6
kmin-jeong <desk535353@gmail.com>
2022-06-25 17:45:53 +0900

    add:

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import { Stack } from './Stack/stackflow';
 
 function App() {
--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import "@stackflow/basic-ui/index.css";
 import App from './App';
 
